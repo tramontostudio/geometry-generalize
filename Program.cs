b61ce2fcfea751dd20ba128d4e599d86657aafe5
tramontostudio <39035953+tramontostudio@users.noreply.github.com>
2020-05-31 19:01:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generalizator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
