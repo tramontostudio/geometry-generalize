6ff5f9dcea19921509ff91b14a733384317c8135
tramontostudio <39035953+tramontostudio@users.noreply.github.com>
2020-06-04 22:46:54 +0200

    added multiple features handling

--- Program.cs
+++ Program.cs
@@ -17,16 +17,16 @@ namespace generalizator
 {
     class Program
     {
-        public static void CreateShpFile(string type, Coordinate[] coords, string path)
-        {
-            // Wkt in 4326 SRID (WGS84)
-            //var wkt = "POLYGON ((-86.7605020509258 41.5101338613656, -86.7604972038273 41.5100611525915, -86.7604971708084 41.5100606308085, -86.7604611720717 41.5094596307695, -86.7604611426546 41.5094591103497, -86.7604291439208 41.5088571103154, -86.760429130715 41.508856853856, -86.7603991319814 41.5082548538241, -86.7603991259966 41.5082547317887, -86.7603701303631 41.5076537960468, -86.7603401446338 41.5070530565908, -86.7603071566895 41.5064532528163, -86.7603071500912 41.506453131098, -86.7602814240795 41.5059715533315, -86.7605549835241 41.5059607024218, -86.7605808466407 41.5064448078787, -86.760613844555 41.5070447469854, -86.7606138651484 41.5070451395365, -86.7606438664126 41.5076461395046, -86.7606438727239 41.5076462680791, -86.7606728710439 41.5082472070294, -86.7607028628788 41.5088490177453, -86.7607348434949 41.5094506292495, -86.7607708135428 41.5100511081057, -86.760776407335 41.5101350123382, -86.7605020509258 41.5101338613656))";
 
+        public static Feature CreateFeature(Coordinate[] coords)
+        {
             var wkt = "LINESTRING (";
-            for(int i=0; i<coords.Count(); i++)
+            for (int i = 0; i < coords.Count(); i++)
             {
-                if (i > 0) wkt += ", ";
-                wkt += (string)(coords[i].X).ToString().Replace(",", ".").Replace(".", ".") + " " + coords[i].Y.ToString().Replace(",", ".").Replace(".", ".");
+                if (i > 0) { wkt += ", "; }
+                wkt += (string)(coords[i].X).ToString().Replace(",", ".").Replace(".", ".") 
+                    + " " 
+                    + coords[i].Y.ToString().Replace(",", ".").Replace(".", ".");
             }
             wkt += ")";
 
@@ -35,18 +35,41 @@ namespace generalizator
 
             var geometry = wktReader.Read(wkt);
 
-            Debug.WriteLine($"Geometry Type: {geometry.GeometryType}");
-            Debug.WriteLine($"Shapefile Type: {Shapefile.GetShapeType(geometry)}");
+            var attributesTable = new AttributesTable();
+
+            return new Feature(geometry, attributesTable);
+        }
+
+        public static void CreateShpFile(List<IFeature> features, string path)
+        {
+            //// Wkt in 4326 SRID (WGS84)
+            ////var wkt = "POLYGON ((-86.7605020509258 41.5101338613656, -86.7604972038273 41.5100611525915, -86.7604971708084 41.5100606308085, -86.7604611720717 41.5094596307695, -86.7604611426546 41.5094591103497, -86.7604291439208 41.5088571103154, -86.760429130715 41.508856853856, -86.7603991319814 41.5082548538241, -86.7603991259966 41.5082547317887, -86.7603701303631 41.5076537960468, -86.7603401446338 41.5070530565908, -86.7603071566895 41.5064532528163, -86.7603071500912 41.506453131098, -86.7602814240795 41.5059715533315, -86.7605549835241 41.5059607024218, -86.7605808466407 41.5064448078787, -86.760613844555 41.5070447469854, -86.7606138651484 41.5070451395365, -86.7606438664126 41.5076461395046, -86.7606438727239 41.5076462680791, -86.7606728710439 41.5082472070294, -86.7607028628788 41.5088490177453, -86.7607348434949 41.5094506292495, -86.7607708135428 41.5100511081057, -86.760776407335 41.5101350123382, -86.7605020509258 41.5101338613656))";
+
+            //var wkt = "LINESTRING (";
+            //for(int i=0; i<coords.Count(); i++)
+            //{
+            //    if (i > 0) wkt += ", ";
+            //    wkt += (string)(coords[i].X).ToString().Replace(",", ".").Replace(".", ".") + " " + coords[i].Y.ToString().Replace(",", ".").Replace(".", ".");
+            //}
+            //wkt += ")";
+
+            //var geomFactory = new GeometryFactory(new PrecisionModel(), 4326);
+            //var wktReader = new WKTReader(geomFactory);
+
+            //var geometry = wktReader.Read(wkt);
+
+            ////Debug.WriteLine($"Geometry Type: {geometry.GeometryType}");
+            ////Debug.WriteLine($"Shapefile Type: {Shapefile.GetShapeType(geometry)}");
 
             
-            IPolygon geomPolygon = geometry as IPolygon;
+            ////IPolygon geomPolygon = geometry as IPolygon;
 
 
-            var attributesTable = new AttributesTable();
-            var features = new List<IFeature>
-            {
-                new Feature(geometry, attributesTable)
-            };
+            //var attributesTable = new AttributesTable();
+            //var features = new List<IFeature>
+            //{
+            //    new Feature(geometry, attributesTable)
+            //};
 
             // Create the directory where we will save the shapefile
             var shapeFilePath =  path;
@@ -239,15 +262,18 @@ namespace generalizator
             ReadArgs(args, ref inputFile, ref outputFile, ref corridorWidth);
 
             FeatureCollection collection = ReadShapeFile(inputFile);
+            FeatureCollection newCollection = new FeatureCollection();
+            List<IFeature> newFeatures = new List<IFeature>();
 
-            for(int i = 0; i< collection.Count; i++)
+            for (int i = 0; i< collection.Count; i++)
             {
                 Feature feature = (Feature)collection[i];
                 Geometry geometry = collection[i].Geometry;
-                //Coordinate[] coordinates = collection[i].Geometry.Coordinates;
                 Coordinate[] newCoords = DouglasGeneralize(geometry, corridorWidth);
-                CreateShpFile("POLYLINE", newCoords, outputFile);
+                newFeatures.Add(CreateFeature(newCoords));
+                
             }
+            CreateShpFile(newFeatures, outputFile);
         }
     }
 }
